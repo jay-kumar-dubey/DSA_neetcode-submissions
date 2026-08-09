0dabee7832977681d2b74768ca52c5292b0ce2a3
Jay kumar Dubey <148194884+jay-kumar-dubey@users.noreply.github.com>
2026-08-09 22:40:00 +0530

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hz = {}
+
+        res = 0
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            hz[s[r]] = hz.get(s[r],0) + 1
+            maxf = max(maxf, hz[s[r]])
+
+            while (r-l+ 1) - max(hz.values()) > k:
+                hz[s[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+
+        return res
+        
